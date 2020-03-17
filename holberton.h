7ecfdd6b722bae3d5387f27afe6e03906a23ddf1
Matt Salem <msalem311@aol.com>
2020-03-17 04:45:57 +0000

    header file

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _strlen(char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, char *src);
+
+#endif /* PRINTF_H */
