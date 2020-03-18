668842a6496f59c0abc850379fc26345a166542f
AmilcarArmmand <armmand@gmail.com>
2020-03-18 01:27:47 +0000

    Added descriptions to functions and parameters

--- _itoa.c
+++ _itoa.c
@@ -3,6 +3,13 @@
 #include <stdbool.h>
 #include "holberton.h"
 
+/**
+ * reverse - a function that reverses a string
+ * @str: the string being reversed
+ * @len: the length of the string
+ *
+ * Return: the string version of the number
+ */
 void reverse(char *str, int len)
 {
 	int i = 0, j = len - 1, temp;
@@ -18,9 +25,12 @@ void reverse(char *str, int len)
 }
 
 /**
+ * _itoa - a function that prints converts an integer to a string
+ * @num: the number to convert
+ * @str: the width of the string buffer to write the number into
+ * @base: the number base of the number to be converted
  *
- *
- *
+ * Return: the string version of the number
  */
 char *_itoa(int num, char *str, int base)
 {
