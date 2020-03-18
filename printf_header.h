1b9e2fa59215566b0854df5c5efd74c2439a0cf3
AmilcarArmmand <armmand@gmail.com>
2020-03-18 03:27:24 +0000

    Updated man page and deleted unnecessary printf.c and printf_header.h files

--- printf_header.h
+++
@@ -1,7 +0,0 @@
-#infndef PRINTF_HEADER_H
-#define PRINTF_HEADER_H
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest char *src);
-void _printf(const char *format, ...)
-#endif
